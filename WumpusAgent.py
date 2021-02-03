6b6228a48d0c610537bc4837740f459a7fabc373
NickFalse <ntrue@smcm.edu>
2021-02-02 19:11:21 -0500

    update git ignore and agent for oop

--- WumpusAgent.py
+++ WumpusAgent.py
@@ -1,12 +1,18 @@
-global i
-i = 0
-def __init__(self):
-    print("initing")
-
+class WumpusAgent:
+    def __init__(self):
+        print("initing")
+    def setParams(self, gametype, numarrows, numwumpi):
+        self.gametype = gametype
+        self.numarrows = numarrows
+        self.numwumpi = numwumpi
+    def getMove(self, percept):
+        print("percept", percept)
+        return("N")
+######################################## all agent code goes above, below just allows oop because lrn2code
+global ag
+ag = WumpusAgent()
 def setParams(gametype, numarrows, numwumpi):
-    print("Hello")
+    ag.setParams(gametype, numarrows, numwumpi)
 def getMove(percept):
-    global i
-    i = i+1
-    print("percept", percept,i)
-    return("N")
+    return ag.getMove(percept)
+    
