a95a34b2f46628cf0e2fb9d8221a2659001114f2
NickFalse <ntrue@smcm.edu>
2021-02-16 16:53:04 -0500

    trim and comments on agent

--- WumpusAgent.py
+++ WumpusAgent.py
@@ -10,7 +10,6 @@ class WumpusAgent:
         self.move = ''
         self.hasGold = False
         self.kills = 0
-        self.undoing = False
         self.pathing = False
         self.path = []
         self.riskTolerance = .1
@@ -19,10 +18,6 @@ class WumpusAgent:
         self.waitSkip=False #Dev option, Default to False used to skip 20k wait on ui
     def getMemory(self):
         return self.memMap
-    def xyToStr(self,x,y):
-        return str(x)+","+str(y)
-    def posStr(self):
-        return self.xyToStr(self.x,self.y)
 ############################travel methods
     def goNorth(self):
         self.y += 1
@@ -62,40 +57,30 @@ class WumpusAgent:
         self.path=[]
     def climbOut(self):
         self.move = "C"
-    def undoAll(self):
-        self.undoMoves(len(self.moves))
-    def undoMoves(self,num):
-        self.undoing = True
-        self.undoStart = len(self.moves)-1
-        self.undoCount = 0
-        self.undoFor = num
-        self.undo()
-    def getOpposite(self, move):
-        undoDict = {"N":"S","S":"N","E":"W","W":"E"}
-        return undoDict[move]
-    def undo(self):
-        self.move = self.getOpposite(self.moves[self.undoStart-self.undoCount])
-        self.undoCount += 1
-        if self.undoCount == self.undoFor:
-            self.undoing = False
+
+    #sets all flags and vars to follow path then does first step
     def followPath(self,path):
-        #print("Follow path:",self.memMap.pathToString(path))
         self.path=path
         self.pathing = True
         self.followStep()
+
+    #individual step in following a path
     def followStep(self):
-        if self.x==0 and self.y==0 and self.hasGold:
+        if self.x==0 and self.y==0 and self.hasGold:#dip if home with gold
             self.climbOut()
             return
-        self.letterToMove(self.path.pop())
-        if len(self.path)==0:
+        self.letterToMove(self.path.pop())#move
+        if len(self.path)==0:#if done pathing, change flag
             self.pathing = False
+    
+    #reverses the input
     def reverseIn(self,inp):
         d = inp
         d.reverse()
         return d
+
+    #helper function, calls a move function from a direction   
     def letterToMove(self,s):
-        #print("letter:",s)
         if s=="N":
             self.goNorth()
         if s=="E":
@@ -104,6 +89,9 @@ class WumpusAgent:
             self.goSouth()
         if s=="W":
             self.goWest()
+
+    #go to the safest tile, typically unexplored. will check a larger and larger area for
+    #a tile with a low enough risk, risk allowed goes up the more checks are done
     def goSafest(self,avoidExplored:bool=True,tolerance:float=0.1,checks:int=25):
         l = self.memMap.getNearestUnexploredEdges(self.memMap.getTile(self.x,self.y),2,tolerance,checks)
         #print(l)
@@ -112,32 +100,34 @@ class WumpusAgent:
             xLen = self.memMap.maxX-self.memMap.minX
             yLen = self.memMap.maxY-self.memMap.minY
             bigLen = xLen if xLen>yLen else yLen
-            bigLen = 2 if bigLen==0 else bigLen
+            bigLen = 10 if bigLen<10 else bigLen
             self.goSafest(True,tolerance+(float(checks)/(bigLen*bigLen)),checks*2)#tune this to reduce how long it gets stuck in weird situations
             return
         path = self.memMap.getPathTo(self.memMap.getTile(self.x,self.y),l[0])
         self.followPath(self.memMap.pathToMoves(path))
+    
+    #called on new game, resets everything
     def setParams(self, gametype, numarrows, numwumpi):
-        self.explored = set()
-        self.moves = ['init']
-        self.memMap = MemoryMap()
-        self.memMap.moving=True if gametype==2 else False
-        self.perceptMemory = dict()
+        self.explored = set()#none explored
+        self.moves = ['init']#first mover
+        self.memMap = MemoryMap()#new memories
+        self.memMap.moving=True if gametype==2 else False#input handling on movement
         self.x = 0
         self.y = 0
-        self.move = ''
+        self.move = ''#holds move
         self.hasGold = False
-        self.kills = 0
-        self.undoing = False
+        self.kills = 0#wumpi killed
         self.moving = self.memMap.moving
         self.numarrows = numarrows if numarrows>-1 else 1
         self.numwumpi = numwumpi if numarrows>-1 else 1
-        self.devMode=True
-        self.pathing=False
+        self.devMode=True#enables remote
+        self.pathing=False#flag for if currently following a path
         self.moveCount=0
-    def skipWait(self,b:bool):
+
+    def skipWait(self,b:bool):#function to override wait
         self.waitSkip = b
-    def getMove(self, percept):
+
+    def getMove(self, percept):#gets move from agent
         if "U" in percept:#handle bump and adjust coords
             last = self.moves[-1]#get last move
             if last == "N":#if dir, reverse that coord chance
@@ -158,25 +148,23 @@ class WumpusAgent:
         #especially useful on moving wumpi where risk moves around
 
         if(self.moving and self.moveCount<20000 and not self.waitSkip):#moving wumpi are much denser against the wall as game goes on, this helps us avoid them
+            #this code is disabled on waitSkip being true, this is just to make it better on the ui
             self.shootNorth()
             self.moveCount+=1
             #print(self.moveCount)
             return self.move
-        if self.pathing:
+
+        if self.pathing:#if following a path
             self.followStep()
             return self.move
-        if self.undoing:##keep for backtrack support
-            self.undo()
-            return self.move
+
         if self.hasGold and not self.pathing:
             self.followPath(self.memMap.pathToMoves(self.reverseIn(self.memMap.getPathTo(self.memMap.map[(0,0)],self.memMap.map[(self.x,self.y)]))))#path from spawn to agent
             #this reversed approach shows to be 25% faster on average
-            return self.move
-        ###self.logPercepts(stench, breeze, glitter, bump, scream)#data gathering function ###move to mem map
+            return self.move        
         
         
-        
-        if self.devMode:
+        if self.devMode:#remote control for testing or just swag
             if "1" in percept:
                 self.goNorth()
                 return(self.move)
@@ -189,8 +177,9 @@ class WumpusAgent:
             elif "4" in percept:
                 self.goWest()
                 return(self.move)
-        #self.goNorth()
+        
         self.goSafest()
+        
         return(self.move)
 ######################################## all agent code goes above, below just allows oop because lrn2code
 global ag
